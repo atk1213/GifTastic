b078ba9364f993ec1d1de339d576248217fd7859
Anthony Kim <akim2214@gmail.com>
2018-01-07 20:37:49 -0800

    added functional javascript need to correct input function for enter/return key press

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var topics = ["taekwondo", "jiu-jitsu", "judo", "karate", "boxing", "krav maga"];
+  var topics = ["taekwondo", "jiu-jitsu", "judo", "karate", "boxing", "krav maga", "silat"];
 
   function renderButtons() {
     $("#button-list").empty();
@@ -9,12 +9,27 @@ $(document).ready(function () {
       a.attr("data-name", topics[i]);
       a.text(topics[i]);
       $("#button-list").append(a);
-
     }
   }
   renderButtons();
+  $("button").css({"margin-right":"5px"});
 
-  $(document).on("click", ".martial-art", function () {
+  $(document).on("click", "#search-button", function () {
+    if ($("#search").val().trim() == "") {
+      alert("Please type something to search");
+    }
+    else {
+      var userSearch = $("#search").val().trim();
+      topics.push(userSearch);
+      $("#search").val("");
+      renderButtons();
+      $("button").css({"margin":"5px", "padding":"5px"});
+      //enter key should submit and render buttons too...
+    }
+
+  });
+
+  $(document).on("click", ".martial-art", function find() {
     var artSelection = $(this).html();
     var apiKey = "vE4IPGS5YdmvKK7Bc8HXBXQincivM9Vi";
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + artSelection + "&api_key=" + apiKey + "&limit=10";
@@ -22,17 +37,41 @@ $(document).ready(function () {
     $.ajax({
       url: queryUrl,
       method: "GET"
-    }).done(function(response) {
-
+    }).done(function (response) {
       var results = response.data;
-      console.log(results);
       $("#gif-gallery").empty();
-      // for (var j = 0; j < results.length; j++) {
-      //   var searchedImage = $("<div>");
-      //   var displayImage = results[j].images
-      // }
+      for (var j = 0; j < results.length; j++) {
+        var rating = results[j].rating;
+        var displayRating = $("<p>").html("Rating: " + rating);
+        $("#gif-gallery").append(displayRating);
+        $("p").css({"display":"inline-block", "clear":"both"});
+
+        var searchedImage = $("<div>");
+        var stillImage = results[j].images.fixed_width_still.url;
+        var animateImage = results[j].images.fixed_width.url;
+        var animateGif = $("<img>").attr("src", stillImage).attr("data-still", stillImage).attr("data-animate", animateImage);
+        animateGif.attr("data-state", "still");
+        $("#gif-gallery").append(animateGif);
+        $("img").css({"margin":"5px"});
+        animateGif.on("click", startStop);
+      }
     })
+
+    function startStop() {
+      var state = $(this).attr("data-state");
+      if (state == "still") {
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+      }
+      else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+      }
+    }
+
+
   });
+
 
 
 })
